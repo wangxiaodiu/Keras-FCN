67025cf1203312fe51efa646239d839310a9e7bb
Liang Niu <niuliang3@gmail.com>
2018-04-20 16:10:12 -0400

    introduce PASCAL config into train_aff.py

--- train_aff.py
+++ train_aff.py
@@ -21,46 +21,43 @@ from utils.SegDataGenerator import *
 
 
 if __name__ == '__main__':
-    # model_name = 'AtrousFCN_Resnet50_16s'
-    #model_name = 'Atrous_DenseNet'
-    model_name = 'DenseNet_FCN'
-    # model_name = 'FCN_Resnet50_32s'
-    batch_size = 2
-    batchnorm_momentum = 0.95
-    epochs = 450
-    lr_base = 0.2 * (float(batch_size) / 4)
-    lr_power = float(1)/float(30)
-    resume_training=False
-    weight_decay = 0.0001/2
-    target_size = (320, 320)
     dataset = 'IIT-AFF'
-    if dataset == 'VOC2012_BERKELEY':
-        # pascal voc + berkeley semantic contours annotations
-        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        # train_file_path = os.path.expanduser('~/.keras/datasets/oneimage/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_val.txt')
-        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
-        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/combined_annotations')
-    if dataset is 'VOC2012':
-        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        # train_file_path = os.path.expanduser('~/.keras/datasets/oneimage/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt')
-        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
-        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/SegmentationClass')
-        classes = 21
-        class_weight = None
-    elif dataset is 'COCO':
-        train_file_path = os.path.expanduser('~/.keras/datasets/coco/annotations/train2014.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        # train_file_path = os.path.expanduser('~/.keras/datasets/oneimage/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        val_file_path   = os.path.expanduser('~/.keras/datasets/coco/annotations/test2014.txt')
-        data_dir        = os.path.expanduser('~/.keras/datasets/coco/train2014')
-        label_dir       = os.path.expanduser('~/.keras/datasets/coco/seg_mask/train2014')
-        stats_file = os.path.expanduser('~/.keras/datasets/coco/seg_mask/train2014/image_segmentation_class_stats.json')
-        classes = 91
-        # class_weight = data_coco.class_weight(image_segmentation_stats_file=stats_file)
-        class_weight = None
+    CONFIG = 'PASCAL' # Choose pre-configured set ('Pascal' -> train.py, 'COCO' -> train_coco.py)
+
+    ###########################################
+    if CONFIG == 'PASCAL':
+        model_name = 'AtrousFCN_Resnet50_16s'
+        # model_name = 'Atrous_DenseNet'
+        # model_name = 'DenseNet_FCN'
+        batch_size = 16
+        batchnorm_momentum = 0.95
+        epochs = 250
+        lr_base = 0.01 * (float(batch_size) / 16)
+        lr_power = 0.9
+        resume_training = False
+        if model_name is 'AtrousFCN_Resnet50_16s':
+            weight_decay = 0.0001 / 2
+        else:
+            weight_decay = 1e-4
+    elif CONFIG == 'COCO':
+        # model_name = 'AtrousFCN_Resnet50_16s'
+        #model_name = 'Atrous_DenseNet'
+        model_name = 'DenseNet_FCN'
+        # model_name = 'FCN_Resnet50_32s'
+        batch_size = 2
+        batchnorm_momentum = 0.95
+        epochs = 450
+        lr_base = 0.2 * (float(batch_size) / 4)
+        lr_power = float(1)/float(30)
+        resume_training=False
+        weight_decay = 0.0001/2
+        target_size = (320, 320)
+    else:
+        print("Config not set correctly, check the code.")
+        exit(1)
+    ##########################################
     if dataset == 'IIT-AFF':
-        path_prefix = '/home/niu/Liang_Niu3/'
+        path_prefix = '/home/niu/Liang_Niu3/IIT_Affordances_2017/'
         train_file_path = os.path.join(path_prefix, 'fcn_train_and_val.txt')
         val_file_path   = os.path.join(path_prefix, 'fcn_val.txt')
         data_dir        = os.path.join(path_prefix, 'rgb_origin')
@@ -87,7 +84,7 @@ if __name__ == '__main__':
           val_file_path,
           data_dir,
           label_dir,
-          target_size=target_size,
+          # target_size=target_size,
           batchnorm_momentum=batchnorm_momentum,
           resume_training=resume_training,
           class_weight=class_weight,
