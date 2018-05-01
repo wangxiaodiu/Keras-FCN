123317ad6a70c3cdee5e330b8f9a7e65213443dc
Liang Niu <niuliang3@gmail.com>
2018-05-01 17:56:20 -0400

    aff model done

--- train_aff.py
+++ train_aff.py
@@ -22,14 +22,45 @@ from utils.SegDataGenerator import *
 
 if __name__ == '__main__':
     dataset = 'IIT-AFF'
-    CONFIG = 'PASCAL' # Choose pre-configured set ('Pascal' -> train.py, 'COCO' -> train_coco.py)
+    CONFIG = 'EXP' # Choose pre-configured set ('Pascal' -> train.py, 'COCO' -> train_coco.py)
 
     ###########################################
-    if CONFIG == 'PASCAL':
-        model_name = 'AtrousFCN_Resnet50_16s'
+
+    if CONFIG == 'EXP':
+        model_name = 'AtrousFCN_Resnet50_Aff'
+        if True: # for test
+            input_shape = (512,512,3)
+            weight_decay = 1e-4
+            batchnorm_momentum = 0.95
+            print('1')
+            seg_classes = 21
+            aff_classes = 10
+            model = globals()[model_name](weight_decay=weight_decay,
+                                  input_shape=input_shape,
+                                  batch_momentum=batchnorm_momentum,
+                                  seg_classes=seg_classes,
+                                  aff_classes=aff_classes)
+            print('2')
+            model.summary()
+            exit(0)
         # model_name = 'Atrous_DenseNet'
         # model_name = 'DenseNet_FCN'
         batch_size = 16
+        batchnorm_momentum = 0.95
+        epochs = 250
+        lr_base = 0.01 * (float(batch_size) / 16)
+        lr_power = 0.9
+        resume_training = False
+        if model_name is 'AtrousFCN_Resnet50_16s':
+            weight_decay = 0.0001 / 2
+        else:
+            weight_decay = 1e-4
+    elif CONFIG == 'PASCAL':
+        model_name = 'AtrousFCN_Resnet50_16s'
+        # model_name = 'Atrous_DenseNet'
+        # model_name = 'DenseNet_FCN'
+        # batch_size = 16
+        batch_size = 8
         batchnorm_momentum = 0.95
         epochs = 250
         lr_base = 0.01 * (float(batch_size) / 16)
@@ -58,7 +89,7 @@ if __name__ == '__main__':
     ##########################################
     if dataset == 'IIT-AFF':
         path_prefix = '/home/niu/Liang_Niu3/IIT_Affordances_2017/'
-        train_file_path = os.path.join(path_prefix, 'fcn_train_and_val.txt')
+        train_file_path = os.path.join(path_prefix, 'fcn_test.txt')
         val_file_path   = os.path.join(path_prefix, 'fcn_val.txt')
         # data_dir        = os.path.join(path_prefix, 'rgb_origin')
         data_dir        = os.path.join(path_prefix, 'rgb') # 512x512 images
@@ -86,7 +117,7 @@ if __name__ == '__main__':
           val_file_path,
           data_dir,
           label_dir,
-          # target_size=target_size,
+          target_size=target_size,
           batchnorm_momentum=batchnorm_momentum,
           resume_training=resume_training,
           class_weight=class_weight,
