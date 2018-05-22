852d77eea41659901a0aa80be6ef382a2eaaa7fa
Liang Niu <niuliang3@gmail.com>
2018-05-22 13:43:48 -0400

    for sync from 009 server to laptop

--- train.py
+++ train.py
@@ -320,9 +320,10 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
     model.save_weights(save_path+'/model.hdf5')
 
 if __name__ == '__main__':
-    model_name = 'AtrousFCN_Resnet50_16s'
+    # model_name = 'AtrousFCN_Resnet50_16s'
     #model_name = 'Atrous_DenseNet'
     #model_name = 'DenseNet_FCN'
+    model_name = 'FCN_Resnet50_32s'
     batch_size = 16
     batchnorm_momentum = 0.95
     epochs = 250
