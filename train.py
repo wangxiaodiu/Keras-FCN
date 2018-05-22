49d7b74d33e6a07a83c393db34abe62edfb9927b
Liang Niu <niuliang3@gmail.com>
2018-05-22 13:40:18 -0400

    add train_seg_aff() to train.py

--- train.py
+++ train.py
@@ -17,7 +17,159 @@ from utils.loss_function import *
 from utils.metrics import *
 from utils.SegDataGenerator import *
 import time
+import datetime
 
+def train_seg_aff(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
+                  model_name, train_file_path, val_file_path,
+                  data_dir, aff_label_dir, semantic_label_dir,
+                  target_size=None, batchnorm_momentum=0.9,
+                  resume_training=False, class_weight=None, dataset='VOC_AFF',
+                  loss_fn=softmax_sparse_crossentropy_ignoring_last_label,
+                  metrics={"seg":sparse_accuracy_ignoring_last_label, "aff":sparse_accuracy_ignoring_last_label},
+                  loss_shape=None,
+                  label_suffix='.png',
+                  data_suffix='.jpg',
+                  ignore_label=255,
+                  label_cval=255,
+                  log_dir='logs'
+                  ):
+    if target_size:
+        input_shape = target_size + (3,)
+    else:
+        input_shape = (None, None, 3)
+    batch_shape = (batch_size,) + input_shape
+
+    ###########################################################
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    save_path = os.path.join(current_dir, 'Models/' + model_name)
+    if os.path.exists(save_path) is False:
+        os.mkdir(save_path)
+
+    # ###############learning rate scheduler####################
+    def lr_scheduler(epoch, mode='power_decay'):
+        '''if lr_dict.has_key(epoch):
+            lr = lr_dict[epoch]
+            print 'lr: %f' % lr'''
+
+        if mode is 'power_decay':
+            # original lr scheduler
+            lr = lr_base * ((1 - float(epoch)/epochs) ** lr_power)
+        if mode is 'exp_decay':
+            # exponential decay
+            lr = (float(lr_base) ** float(lr_power)) ** float(epoch+1)
+        # adam default lr
+        if mode is 'adam':
+            lr = 0.001
+
+        if mode is 'progressive_drops':
+            # drops as progression proceeds, good for sgd
+            if epoch > 0.9 * epochs:
+                lr = 0.0001
+            elif epoch > 0.75 * epochs:
+                lr = 0.001
+            elif epoch > 0.5 * epochs:
+                lr = 0.01
+            else:
+                lr = 0.1
+
+        print('lr: %f' % lr)
+        return lr
+    scheduler = LearningRateScheduler(lr_scheduler)
+
+    # ###################### make model ########################
+    checkpoint_path = os.path.join(save_path, 'checkpoint_weights.hdf5')
+
+    model = globals()[model_name](weight_decay=weight_decay,
+                                  input_shape=input_shape,
+                                  batch_momentum=batchnorm_momentum,
+                                  classes=classes)
+
+    # ###################### optimizer ########################
+    optimizer = SGD(lr=lr_base, momentum=0.9)
+    # optimizer = Nadam(lr=lr_base, beta_1 = 0.825, beta_2 = 0.99685)
+
+    model.compile(loss={'seg':loss_fn,'aff':loss_fn}, loss_weights = {'seg':0.3, 'aff':0.7},
+                  optimizer=optimizer,
+                  metrics=metrics)
+    if resume_training:
+        model.load_weights(checkpoint_path, by_name=True)
+    model_path = os.path.join(save_path, "model.json")
+    # save model structure
+    f = open(model_path, 'w')
+    model_json = model.to_json()
+    f.write(model_json)
+    f.close()
+    img_path = os.path.join(save_path, "model.png")
+    # #vis_util.plot(model, to_file=img_path, show_shapes=True)
+    model.summary()
+
+    # lr_reducer      = ReduceLROnPlateau(monitor=softmax_sparse_crossentropy_ignoring_last_label, factor=np.sqrt(0.1),
+    #                                     cooldown=0, patience=15, min_lr=0.5e-6)
+    # early_stopper   = EarlyStopping(monitor=sparse_accuracy_ignoring_last_label, min_delta=0.0001, patience=70)
+    # callbacks = [early_stopper, lr_reducer]
+    callbacks = [scheduler]
+
+    # ####################### tfboard ###########################
+    time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
+    if K.backend() == 'tensorflow':
+        TB_dir = os.path.join(save_path, f"{log_dir}/{time_str}")
+        tensorboard = TensorBoard(log_dir=TB_dir, histogram_freq=0, write_graph=True)
+        callbacks.append(tensorboard)
+    # ################### checkpoint saver#######################
+    checkpoint = ModelCheckpoint(filepath=os.path.join(save_path, f'checkpoint_weights_{time_str}.hdf5'), save_weights_only=True)#.{epoch:d}
+    callbacks.append(checkpoint)
+    # set data generator and train
+    train_datagen = SegAffDataGenerator(zoom_range=[0.5, 2.0],
+                                        zoom_maintain_shape=True,
+                                        crop_mode='random',
+                                        crop_size=target_size,
+                                        # pad_size=(505, 505),
+                                        rotation_range=0.,
+                                        shear_range=0,
+                                        horizontal_flip=True,
+                                        channel_shift_range=20.,
+                                        fill_mode='constant',
+                                        label_cval=label_cval)
+    # val_datagen = SegDataGenerator()
+
+    def get_file_len(file_path):
+        fp = open(file_path)
+        lines = fp.readlines()
+        fp.close()
+        return len(lines)
+
+    # from Keras documentation: Total number of steps (batches of samples) to yield from generator before declaring one epoch finished
+    # and starting the next epoch. It should typically be equal to the number of unique samples of your dataset divided by the batch size.
+    steps_per_epoch = int(np.ceil(get_file_len(train_file_path) / float(batch_size)))
+
+    history = model.fit_generator(
+        generator=train_datagen.flow_from_directory(
+            file_path=train_file_path,
+            data_dir=data_dir, data_suffix=data_suffix,
+            aff_label_dir=aff_label_dir, semantic_label_dir=semantic_label_dir,
+            label_suffix=label_suffix,
+            classes=classes,
+            target_size=target_size, color_mode='rgb',
+            batch_size=batch_size, shuffle=True,
+            loss_shape=loss_shape,
+            ignore_label=ignore_label,
+            # save_to_dir='Images/'
+        ),
+        steps_per_epoch=steps_per_epoch,
+        epochs=epochs,
+        callbacks=callbacks,
+        workers=4,
+        # validation_data=val_datagen.flow_from_directory(
+        #     file_path=val_file_path, data_dir=data_dir, data_suffix='.jpg',
+        #     label_dir=label_dir, label_suffix='.png',classes=classes,
+        #     target_size=target_size, color_mode='rgb',
+        #     batch_size=batch_size, shuffle=False
+        # ),
+        # nb_val_samples = 64
+        class_weight=class_weight
+    )
+
+    model.save_weights(save_path+'/model.hdf5')
 
 def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
           model_name, train_file_path, val_file_path,
